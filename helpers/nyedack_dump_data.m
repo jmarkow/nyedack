cc9790daf0592fe542cfcb528e11b7dd85cf2516
Jeff Markowitz <markowitzmeister@gmail.com>
2015-01-14 16:23:59 -0500

    Cleaning up preview

--- helpers/nyedack_dump_data.m
+++ helpers/nyedack_dump_data.m
@@ -1,4 +1,4 @@
-function dump_data(obj,event,save_dir,folder_format,out_dir,file_basename,file_format,logfile,actualrate,channel_labels,preview_figure,channel_axis,channel_plot,dump_samples)
+function dump_data(obj,event,dump_samples,save_dir,folder_format,out_dir,file_basename,file_format,logfile,actualrate,channel_labels,preview_figure,channel_axis,channel_plot)
 
 % basically, a circular buffer is used!
 
@@ -13,28 +13,24 @@ global preview_refresh_rate;
 
 available_samples=obj.SamplesAvailable;
 refresh_samples=round((preview_refresh_rate/1e3)*actualrate);
-%%% preview code
 
-disp('test')
-available_samples
-dump_samples
+%%% preview code
 
 if ~isempty(preview_figure) & available_samples<dump_samples & available_samples>refresh_samples
 
 	ylimits=[-preview_voltage_scale/1e6 preview_voltage_scale/1e6];
 	xlimits=[0 preview_refresh_rate/1e3];
 
-	[data]=peekdata(obj,obj.SamplesAvailable);
-    
-    % grab latest segment
-    
-  
-    
-    [nsamples,nchannels]=size(data);
-    data=data(nsamples-refresh_samples+1:nsamples,:);
-    time=[1:refresh_samples]/actualrate;
-	
-    for i=1:length(channel_axis)
+	%[data]=peekdata(obj,obj.SamplesAvailable);
+	data=peakdata(obj,refresh_samples);
+
+	% grab latest segment
+
+	%[nsamples,nchannels]=size(data);
+	%data=data(nsamples-refresh_samples+1:nsamples,:);
+	time=[1:refresh_samples]/actualrate;
+
+	for i=1:length(channel_axis)
 		set(channel_plot(i),'XData',time,'YData',data(:,i));
 		old_xlimits=get(channel_axis(i),'xlim');
 		old_ylimits=get(channel_axis(i),'ylim');
@@ -46,7 +42,7 @@ if ~isempty(preview_figure) & available_samples<dump_samples & available_samples
 		if ~all(ylimits==old_ylimits)
 			set(channel_axis(i),'ylim',ylimits);
 		end
-		
+
 		drawnow;
 	end
 end
@@ -69,8 +65,8 @@ if available_samples>dump_samples
 		save(fullfile(save_dir,datafile_name),'data');
 		fprintf(logfile,'%s saved successfully at %s\n',fullfile(save_dir,datafile_name),datestr(now));
 		disp([ fullfile(save_dir,datafile_name) ' saved successfully at ' datestr(now) ]);
-    catch
-        warning('Could not get data, flushing...');
+	catch
+		warning('Could not get data, flushing...');
 		flushdata(obj);
 	end
 end

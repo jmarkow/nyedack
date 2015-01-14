3dab868c3dff9cf3d77958d006c1a0ee47f0c244
Jeffrey Markowitz <jmarkow@cns.bu.edu>
2015-01-14 15:40:55 -0500

    Changes to preview

--- helpers/nyedack_preview_data.m
+++ helpers/nyedack_preview_data.m
@@ -1,5 +1,6 @@
-function nyedack_preview_data(obj,event,channel_axis,channel_plot)
-
+function nyedack_preview_data(obj,event,channel_axis,channel_plot,actualrate)
+% preview data still not quite working correctly, need some more time with
+% this...
 % basically, a circular buffer is used!
 
 %disp('Dumping data...');
@@ -13,10 +14,10 @@ ylimits=[-preview_voltage_scale/1e6 preview_voltage_scale/1e6];
 xlimits=[0 preview_refresh_rate/1e3];
 
 try
-	[data.voltage,data.time,data.start_time]=getdata(obj,obj.SamplesAvailable);
-    	data.time=data.time-data.time(1);
+	[data]=peekdata(obj,obj.SamplesAvailable);
+    time=[1:length(data)]/actualrate;
 	for i=1:length(channel_axis)
-		set(channel_plot(i),'XData',data.time-data.time(1),'YData',data.voltage(:,i));
+		set(channel_plot(i),'XData',time,'YData',data(:,i));
 		old_xlimits=get(channel_axis(i),'xlim');
 		old_ylimits=get(channel_axis(i),'ylim');
 
@@ -30,9 +31,9 @@ try
 		
 		drawnow;
 	end
-catch 
+catch err
     warning('No samples found, consider decreasing refresh rate');
-    flushdata(obj);	
+    %flushdata(obj);	
 end
 
 % comment this out to approximate a circular buffer
